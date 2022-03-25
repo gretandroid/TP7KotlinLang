280df51f2bdfa947b6d514bdbe41011a3e8a0093
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-03-25 15:18:35 +0100

    pour partager une suggestion 2

--- app/src/main/kotlin/education/cccp/basics/Person.kt
+++ app/src/main/kotlin/education/cccp/basics/Person.kt
@@ -10,4 +10,38 @@ open class Person(
     val age: Int = 0,
     val salary: Double = 0.0,
     val vehicules: List<Vehicule> = mutableListOf()
-)
+){
+    override fun toString(): String {
+        return "Person(id=$id, lastName='$lastName', firstName='$firstName', birthDate=$birthDate, age=$age, salary=$salary, vehicules=$vehicules)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Person
+
+        if (id != other.id) return false
+        if (lastName != other.lastName) return false
+        if (firstName != other.firstName) return false
+        if (birthDate != other.birthDate) return false
+        if (age != other.age) return false
+        if (salary != other.salary) return false
+        if (vehicules != other.vehicules) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + lastName.hashCode()
+        result = 31 * result + firstName.hashCode()
+        result = 31 * result + birthDate.hashCode()
+        result = 31 * result + age
+        result = 31 * result + salary.hashCode()
+        result = 31 * result + vehicules.hashCode()
+        return result
+    }
+
+
+}
